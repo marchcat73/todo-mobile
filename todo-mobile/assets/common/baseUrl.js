178a3d69697341a9097003ecf3921c51c1d7a194
Vitaliy Vanyushin <wm@codehtml.ru>
2024-01-24 15:04:12 +0400

    ios

--- todo-mobile/assets/common/baseUrl.js
+++ todo-mobile/assets/common/baseUrl.js
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 let baseUrl = '';
 
 {
-  Platform.OS === 'android'
+  Platform.OS === 'android' || 'ios'
     ? (baseUrl = 'http://192.168.1.141:4000/graphql')
     : (baseUrl = 'http://localhost:4000/graphql');
 }
